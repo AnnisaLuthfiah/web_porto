a809de5a16ab3cace1130297a411d4a91db74e3c
Annisa Luthfiah <annisaluthfiah67@gmail.com>
2025-04-01 06:51:45 +0000

    Deskripsi perubahan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
 
+Route::resource('projects', ProjectController::class);
 Route::get('/', function () {
     return view('welcome');
 });
